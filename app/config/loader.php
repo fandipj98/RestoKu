e0ee1b3dc24498d39003bf5cfebc4299009faadb
JohnforJC <animeloverz072@gmail.com>
2020-05-14 21:14:39 +0700

    Upload fitur Jonathan

--- app/config/loader.php
+++ app/config/loader.php
@@ -5,9 +5,16 @@ $loader = new \Phalcon\Loader();
 /**
  * We're a registering a set of directories taken from the configuration file
  */
+
+$loader->registerNamespaces([
+    'Phalcon\Db' => APP_PATH . '/library/Phalcon/Db',
+]);
+
 $loader->registerDirs(
     [
         $config->application->controllersDir,
         $config->application->modelsDir
     ]
-)->register();
+);
+    
+$loader->register();
